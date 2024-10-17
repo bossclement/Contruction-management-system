58c092848688187d60f5991a26ad737d9c0967f0
bossclement <bosstclement@gmail.com>
2024-10-17 12:40:52 +0200

    Added newsletter endpoint to record newsletter emails in database

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from datetime import timedelta
 from blueprints.backend.backend import app as backend
 from blueprints.client.client import client
 from blueprints.admin.admin import admin
+from blueprints.backend.database.dao.newsletterDao import NewsLetterDao
+from blueprints.backend.database.models.newsletter import NewsLetter
 
 
 app = Flask(__name__)
@@ -23,6 +25,19 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('pages/contact.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return redirect(url_for('backend.dashboard'))
+
+@app.route('/newletter', methods=["POST"])
+def newletter():
+    email = request.form.get('email', None)
+    if email:
+        newsletter = NewsLetter(email=email)
+        res = NewsLetterDao.create(newsletter=newsletter)
+        flash(res['msg'], 'failed' if not res['status'] else 'success')
+    return redirect(request.referrer or url_for('home'))
 
 if __name__ == '__main__':
     app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=60)
