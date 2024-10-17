0443d871eeb67b7728aa1a18d073909260763f33
bossclement <bosstclement@gmail.com>
2024-10-17 12:52:28 +0200

    Fixed bug of missing tables in database

--- blueprints/backend/database/db.py
+++ blueprints/backend/database/db.py
@@ -1,6 +1,8 @@
 from sqlalchemy.orm import sessionmaker
 from blueprints.backend.database.models.user import User
 from blueprints.backend.database.models.job import Job
+from blueprints.backend.database.models.newsletter import NewsLetter
+from blueprints.backend.database.models.message import Message
 from blueprints.backend.database.database import Base
 from blueprints.backend.database.database import engine
 
